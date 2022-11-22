0ee2375236a0a0c38086c0fb1078f56fa6613849
Rafael Moreira <55898953+vonot16@users.noreply.github.com>
2022-11-21 21:03:26 -0300

    SOA Pattern Added

---
+++ workspace_eclipse/ADS_Padrao_SOA/src/main/java/br/inatel/labs/labads/padraosoa/AdsPadraoSoaApplication.java
@@ -0,0 +1,13 @@
+package br.inatel.labs.labads.padraosoa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdsPadraoSoaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdsPadraoSoaApplication.class, args);
+	}
+
+}
---
+++ workspace_eclipse/ADS_Padrao_SOA/src/main/java/br/inatel/labs/labads/padraosoa/controller/MyMessage.java
@@ -0,0 +1,15 @@
+package br.inatel.labs.labads.padraosoa.controller;
+
+public class MyMessage {
+	private String info;
+
+	public String getInfo() {
+		return info;
+	}
+
+	public void setInfo(String info) {
+		this.info = info;
+	}
+	
+	
+}
